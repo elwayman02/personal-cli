45c5d7148627884237548ef6048cd69178ad2342
Jordan Hawker <hawker.jordan@gmail.com>
2015-08-21 23:50:48 -0700

    Fix test setup

--- tests/tests.js
+++ tests/tests.js
@@ -1,8 +1,7 @@
 // If tests are not running in the browser, these will be undefined
-if (typeof QUnit == 'undefined')
+if (typeof QUnit == 'undefined') {
     QUnit = require('qunit-cli');
-if (typeof Edgar == 'undefined')
-    Edgar = require('./../src/edgar');
+}
 
 var module = QUnit.module;
 var test = QUnit.test;
